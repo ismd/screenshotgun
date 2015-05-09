b08ad470d8908bc96f17ce4c67b054daf806a866
Vladimir Kosteley <zzismd@gmail.com>
2015-05-09 13:34:40 +0600

    msgHandler

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,20 @@
 #include <QDebug>
 #include <QApplication>
 
+void msgHandler(QtMsgType type, const char* msg)
+{
+    const char symbols[] = {'I', 'E', '!', 'X'};
+    QString output = QString("[%1] %2").arg(symbols[type]).arg(msg);
+    std::cerr << output.toStdString() << std::endl;
+
+    if (type == QtFatalMsg) {
+        abort();
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMsgHandler(msgHandler);
     QApplication app(argc, argv);
     QStringList args = app.arguments();
 
