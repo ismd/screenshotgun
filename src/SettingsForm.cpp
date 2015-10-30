6520a6a83b207b52828c0fd145013f73701717d9
Vladimir Kosteley <zzismd@gmail.com>
2015-10-30 23:11:45 +0600

    Filename fix

--- src/SettingsForm.cpp
+++ src/SettingsForm.cpp
@@ -1,7 +1,7 @@
 #include "App.h"
 #include "Autostartup.h"
 #include "SettingsForm.h"
-#include "ui_settings.h"
+#include "ui_Settings.h"
 
 SettingsForm::SettingsForm(App& app) : ui(new Ui::Settings), app_(app) {
     ui->setupUi(this);
--- src/Toolbar.cpp
+++ src/Toolbar.cpp
@@ -1,7 +1,7 @@
 #include <QGraphicsScene>
 #include "App.h"
 #include "Toolbar.h"
-#include "ui_toolbar.h"
+#include "ui_Toolbar.h"
 
 Toolbar::Toolbar(AppView& appView)
     : QWidget(&appView),
