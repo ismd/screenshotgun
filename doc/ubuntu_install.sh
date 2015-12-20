2645633286200f1bd4853380f54668b9bd8372d6
Vladimir Kosteley <zzismd@gmail.com>
2015-12-20 12:32:26 +0600

    Removed unnecessary files

--- doc/ubuntu_install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0989698D105AEEBB
-echo 'deb http://ubuntu.screenshotgun.com trusty main' | sudo tee --append /etc/apt/sources.list > /dev/null
-sudo apt-get update
-sudo apt-get install screenshotgun
