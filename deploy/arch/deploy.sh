d0b3458f15cac7c3146558fddb1cfb90cc53a9c9
Vladimir Kosteley <ismd@yandex-team.ru>
2019-01-04 19:53:40 +0300

    Deploy fix

--- deploy/arch/deploy.sh
+++ deploy/arch/deploy.sh
@@ -3,21 +3,28 @@ BUILD_DIR=/build
 
 mkdir ~/.ssh
 cp $DEPLOY_DIR/aur ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
+
+git config user.email "zzismd@gmail.com"
+git config user.name "Vladimir Kosteley"
 
 cd $BUILD_DIR
 git clone ssh://aur@aur.archlinux.org/screenshotgun-git.git
 cd screenshotgun-git
 
-git config user.email "zzismd@gmail.com"
-git config user.name "Vladimir Kosteley"
-
 cp $DEPLOY_DIR/PKGBUILD PKGBUILD
 AUR_VERSION=`echo $VERSION | sed 's/-/_/g'`
 sed -i -e "s/{AUR_VERSION}/$AUR_VERSION/g" PKGBUILD
 sed -i -e "s/{VERSION}/$VERSION/g" PKGBUILD
-touch .SRCINFO
 makepkg --printsrcinfo > .SRCINFO
-git commit -am "Version: $VERSION"
+
+# Checking build
+cp -r $BUILD_DIR/screenshotgun-git $BUILD_DIR/build
+cd $BUILD_DIR/build
 makepkg -s --needed --noconfirm
+
+# Committing and pushing
+cd $BUILD_DIR/screenshotgun-git
+git commit -am "Version: $VERSION"
 git push
