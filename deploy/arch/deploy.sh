bddb627d057e0a3893ab4c50ea04d5ca5d55383d
Vladimir Kosteley <ismd@yandex-team.ru>
2019-01-04 19:36:29 +0300

    Travis Arch build

--- deploy/arch/deploy.sh
+++ deploy/arch/deploy.sh
@@ -1,9 +1,14 @@
 DEPLOY_DIR=/deploy
 BUILD_DIR=/build
 
-mkdir $BUILD_DIR && cd $BUILD_DIR
+mkdir ~/.ssh
+cp $DEPLOY_DIR/aur ~/.ssh/id_rsa
+ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
+
+cd $BUILD_DIR
 git clone ssh://aur@aur.archlinux.org/screenshotgun-git.git
 cd screenshotgun-git
+
 git config user.email "zzismd@gmail.com"
 git config user.name "Vladimir Kosteley"
 
@@ -12,7 +17,7 @@ AUR_VERSION=`echo $VERSION | sed 's/-/_/g'`
 sed -i -e "s/{AUR_VERSION}/$AUR_VERSION/g" PKGBUILD
 sed -i -e "s/{VERSION}/$VERSION/g" PKGBUILD
 touch .SRCINFO
-chmod 666 .SRCINFO
-sudo -u build bash -c 'makepkg --printsrcinfo > .SRCINFO'
+makepkg --printsrcinfo > .SRCINFO
 git commit -am "Version: $VERSION"
+makepkg -s --needed --noconfirm
 git push
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION=$1
+VERSION=${VERSION#"v"}
 
 # AUR
 docker build --build-arg VERSION=$VERSION $TRAVIS_BUILD_DIR/deploy/arch
