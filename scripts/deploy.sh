caedd765ccfc560dc95bbbc9badd6db8dac25b2f
Vladimir Kosteley <ismd@yandex-team.ru>
2019-01-12 15:14:56 +0300

    Launchpad build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,8 @@ set -o pipefail
 
 VERSION=${1#"v"}
 
+# LAUNCHPAD
+$TRAVIS_BUILD_DIR/deploy/deb/deploy.sh
+
 # AUR
 docker run -e VERSION=$VERSION -v $TRAVIS_BUILD_DIR/deploy:/deploy --privileged base/archlinux /deploy/arch/deploy.sh
