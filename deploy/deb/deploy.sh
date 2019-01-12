8b6ee692516299fdda4efd60910f71f5d2b31d2f
Vladimir Kosteley <ismd@yandex-team.ru>
2019-01-12 15:14:56 +0300

    Launchpad build

---
+++ deploy/deb/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+DEPLOY_DIR=/deploy
+
+mkdir -p $DEPLOY_DIR/launchpad
+
+cd $DEPLOY_DIR/launchpad
+bzr dh-make screenshotgun $VERSION $TRAVIS_BUILD_DIR
+
+cd $DEPLOY_DIR/launchpad/screenshotgun
+rm -rf debian
+cp -r $TRAVIS_BUILD_DIR/deploy/deb/debian debian
+sed -i -e "s/{version}/$VERSION/g" debian/changelog
+sed -i -e "s/{date}/`date +'%a, %d %b %Y %H:%M:%S +0300'`/g" debian/changelog
+
+bzr builddeb -S
+
+cd $DEPLOY_DIR/launchpad/build-area
+pbuilder-dist xenial build screenshotgun_$VERSION-1.dsc
+dput ppa:ismd/screenshotgun screenshotgun_$VERSION-1_source.changes
