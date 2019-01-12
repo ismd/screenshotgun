09a48f480721d08b32b28a867e1adb2290c41d78
Vladimir Kosteley <ismd@yandex-team.ru>
2019-01-12 14:19:56 +0300

    Docker aur

--- deploy/arch/deploy.sh
+++ deploy/arch/deploy.sh
@@ -1,33 +1,17 @@
+#!/bin/bash
 set -e
 set -o pipefail
 
 DEPLOY_DIR=/deploy
 BUILD_DIR=/build
 
-mkdir ~/.ssh
-cp $DEPLOY_DIR/aur ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
+pacman -Sy
+pacman -S --needed --noconfirm base-devel git openssh
 
-cd $BUILD_DIR
-git clone ssh://aur@aur.archlinux.org/screenshotgun-git.git
-cd screenshotgun-git
+useradd -m build
+echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-git config user.email "zzismd@gmail.com"
-git config user.name "Vladimir Kosteley"
+chown -R build $DEPLOY_DIR
+mkdir $BUILD_DIR && chown build $BUILD_DIR
 
-cp $DEPLOY_DIR/PKGBUILD PKGBUILD
-AUR_VERSION=`echo $VERSION | sed 's/-/_/g'`
-sed -i -e "s/{AUR_VERSION}/$AUR_VERSION/g" PKGBUILD
-sed -i -e "s/{VERSION}/$VERSION/g" PKGBUILD
-makepkg --printsrcinfo > .SRCINFO
-
-# Checking build
-cp -r $BUILD_DIR/screenshotgun-git $BUILD_DIR/build
-cd $BUILD_DIR/build
-makepkg -s --needed --noconfirm
-
-# Committing and pushing
-cd $BUILD_DIR/screenshotgun-git
-git commit -am "Version: $VERSION"
-git push
+sudo VERSION=$VERSION -u build $DEPLOY_DIR/arch/deploy_build.sh
