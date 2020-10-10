598081f1a67b9de8fb1f88931bb0f1867f600068
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-11 01:15:28 +0300

    before_install fix

--- scripts/ci/osx/before_install.sh
+++ scripts/ci/osx/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 openssl aes-256-cbc -K $encrypted_33b5e9db54c1_key -iv $encrypted_33b5e9db54c1_iv -in $TRAVIS_BUILD_DIR/scripts/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -d
-tar xvf -C $TRAVIS_BUILD_DIR/scripts/deploy $TRAVIS_BUILD_DIR/scripts/deploy/data.tar
+tar -C $TRAVIS_BUILD_DIR/scripts/deploy -xvf $TRAVIS_BUILD_DIR/scripts/deploy/data.tar
