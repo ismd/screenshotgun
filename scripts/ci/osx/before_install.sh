f0aba8f44183e1756aefab5911345ed2116ef193
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-10 23:55:01 +0300

    Move deploy dir

--- scripts/ci/osx/before_install.sh
+++ scripts/ci/osx/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_55f6cc107eaa_key -iv $encrypted_55f6cc107eaa_iv -in $TRAVIS_BUILD_DIR/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/deploy/data.tar -d
-tar xvf $TRAVIS_BUILD_DIR/deploy/data.tar
+openssl aes-256-cbc -K $encrypted_55f6cc107eaa_key -iv $encrypted_55f6cc107eaa_iv -in $TRAVIS_BUILD_DIR/scripts/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -d
+tar xvf $TRAVIS_BUILD_DIR/scripts/deploy/data.tar
