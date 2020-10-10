45e2a7bb8bb0c9660a428da58f531667c053e604
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-11 01:22:29 +0300

    before_install fix (2)

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 openssl aes-256-cbc -K $encrypted_33b5e9db54c1_key -iv $encrypted_33b5e9db54c1_iv -in $TRAVIS_BUILD_DIR/scripts/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -d
-tar -C $TRAVIS_BUILD_DIR/scripts/deploy -xvf $TRAVIS_BUILD_DIR/scripts/deploy/data.tar
+tar xvf $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -C $TRAVIS_BUILD_DIR/scripts/deploy
