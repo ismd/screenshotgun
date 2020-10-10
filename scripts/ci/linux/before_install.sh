27a63e65b878a338a4305b79da1e0abe442368b8
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-11 00:20:57 +0300

    Deploy

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-openssl aes-256-cbc -K $encrypted_55f6cc107eaa_key -iv $encrypted_55f6cc107eaa_iv -in $TRAVIS_BUILD_DIR/scripts/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -d
+openssl aes-256-cbc -K $encrypted_33b5e9db54c1_key -iv $encrypted_33b5e9db54c1_iv -in $TRAVIS_BUILD_DIR/scripts/deploy/data.tar.enc -out $TRAVIS_BUILD_DIR/scripts/deploy/data.tar -d
 tar xvf $TRAVIS_BUILD_DIR/scripts/deploy/data.tar
