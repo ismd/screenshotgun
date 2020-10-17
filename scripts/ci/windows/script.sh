3964f3a0f85d426bbdac64cb9c372ed95814a0b3
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

--- scripts/ci/windows/script.sh
+++ scripts/ci/windows/script.sh
@@ -2,5 +2,9 @@
 
 mkdir $TRAVIS_BUILD_DIR/build
 cd $TRAVIS_BUILD_DIR/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.13.2\msvc2017" ..
-cmake --build .
+ls -l C:\
+ls -l C:\Qt\5.13.2
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.13.2\msvc2017" ..
+ls -la
+cmake  --build . --config Release
+ls -la
