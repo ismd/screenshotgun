1c7fa88047c8afa64489c101e41dbce651f2afd3
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

--- scripts/ci/windows/script.sh
+++ scripts/ci/windows/script.sh
@@ -3,4 +3,4 @@
 mkdir $TRAVIS_BUILD_DIR/build
 cd $TRAVIS_BUILD_DIR/build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.13.2\msvc2017" ..
-cmake --build .
+cmake --config Release --build .
