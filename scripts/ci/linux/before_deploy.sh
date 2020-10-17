0fc9d9b17f5ab03aa73e4d42b57a6feb5c78f743
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

---
+++ scripts/ci/linux/before_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RELEASE_DIR=$HOME/release
+
+mkdir -p $RELEASE_DIR
+cd $TRAVIS_BUILD_DIR/build
+zip $RELEASE_DIR/screenshotgun_linux.zip screenshotgun
+
+echo "!!!!!!!!!!!!!!!!!!!!!!"
+cd ~
+pwd
