37e6b5c534f343b1f2d440ceaa8650002dfa88b6
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

--- scripts/before_deploy/osx/before_deploy.sh
+++ scripts/before_deploy/osx/before_deploy.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+RELEASE_DIR=$HOME/release/$TRAVIS_OS_NAME
+
+ls -l $TRAVIS_BUILD_DIR/build
+ls -l $TRAVIS_BUILD_DIR/build/screenshotgun.app
+
+mkdir -p $RELEASE_DIR
+cp $TRAVIS_BUILD_DIR/build/screenshotgun.app $RELEASE_DIR/screenshotgun_osx.app
