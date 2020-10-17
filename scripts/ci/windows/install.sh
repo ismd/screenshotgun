dc87cc9b05e2a18436a5fd87da3c8d2ad8dacce3
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

--- scripts/ci/windows/install.sh
+++ scripts/ci/windows/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if [ -z "$(ls -A $HOME/Qt)" ]; then
+choco install zip
+
+if [ -z "$(ls -A /c/Qt)" ]; then
     $HOME/qt-opensource-windows-x86-5.13.2.exe --script $TRAVIS_BUILD_DIR/scripts/ci/windows/scripts/qt_installer_windows.qs
 fi
