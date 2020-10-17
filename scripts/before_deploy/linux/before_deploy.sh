42fdf8e1abde2733b6a9e7fafe1bfa8ec6aded7a
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-17 16:34:37 +0300

    osx, windows deploy

--- scripts/before_deploy/linux/before_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-RELEASE_DIR=$HOME/release/github/linux
-
-mkdir -p $RELEASE_DIR
-cp $TRAVIS_BUILD_DIR/build/screenshotgun $RELEASE_DIR
--- scripts/ci/windows/script.sh
+++ scripts/ci/windows/script.sh
@@ -2,5 +2,8 @@
 
 mkdir $TRAVIS_BUILD_DIR/build
 cd $TRAVIS_BUILD_DIR/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.13.2\msvc2017" ..
-cmake --build .
+ls -l /
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.13.2\msvc2017" ..
+ls -la
+cmake  --build . --config Release --target ALL_BUILD
+ls -la
