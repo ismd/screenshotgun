c7dcc4bd3df9a6adc064642d45e512073086ce7b
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-18 03:24:48 +0300

    asd1

--- scripts/ci/linux/before_deploy.sh
+++ scripts/ci/linux/before_deploy.sh
@@ -5,3 +5,5 @@ RELEASE_DIR=$HOME/release/$TRAVIS_OS_NAME
 mkdir -p $RELEASE_DIR
 cd $TRAVIS_BUILD_DIR/build
 zip $RELEASE_DIR/screenshotgun_linux.zip screenshotgun
+
+cd $RELEASE_DIR
--- scripts/ci/windows/before_deploy.sh
+++ scripts/ci/windows/before_deploy.sh
@@ -5,3 +5,5 @@ RELEASE_DIR=$HOME/release/$TRAVIS_OS_NAME
 mkdir -p $RELEASE_DIR
 cd $TRAVIS_BUILD_DIR/build
 7z a -tzip $RELEASE_DIR/screenshotgun_windows.zip screenshotgun.exe
+
+cd $RELEASE_DIR
