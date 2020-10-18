c4ea958427b89eb7c07a6faddce18c96f7516bdc
Vladimir Kosteley <ismd@yandex-team.ru>
2020-10-18 03:11:28 +0300

    asd

--- scripts/ci/windows/before_deploy.sh
+++ scripts/ci/windows/before_deploy.sh
@@ -4,4 +4,4 @@ RELEASE_DIR=$HOME/release/$TRAVIS_OS_NAME
 
 mkdir -p $RELEASE_DIR
 cd $TRAVIS_BUILD_DIR/build
-zip $RELEASE_DIR/screenshotgun_windows.zip screenshotgun.exe
+7z a -tzip $RELEASE_DIR/screenshotgun_windows.zip screenshotgun.exe
--- scripts/ci/windows/install.sh
+++ scripts/ci/windows/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-choco install zip
-
 if [ -z "$(ls -A /c/Qt)" ]; then
     $HOME/qt-opensource-windows-x86-5.13.2.exe --script $TRAVIS_BUILD_DIR/scripts/ci/windows/scripts/qt_installer_windows.qs
 fi
