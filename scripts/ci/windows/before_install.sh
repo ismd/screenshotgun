99fe59d0c6009cce4d0c9b652309d66f85b87cba
Vladimir Kosteley <zzismd@gmail.com>
2021-02-07 02:29:19 +0300

    windows aqtinstall

--- scripts/ci/windows/before_install.sh
+++ scripts/ci/windows/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 if [ -z "$(ls -A /c/Qt)" ]; then
-    wget -q https://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-windows-x86-5.13.2.exe -P $HOME
+    choco install pip
+    py -m pip install --upgrade pip
+    py -m pip install aqtinstall
+    py -m aqt install --outputdir /c/Qt 5.15.2 windows desktop win64_mingw81
 fi
--- scripts/ci/windows/install.sh
+++ scripts/ci/windows/install.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-
-if [ -z "$(ls -A /c/Qt)" ]; then
-    $HOME/qt-opensource-windows-x86-5.13.2.exe --script $TRAVIS_BUILD_DIR/scripts/ci/windows/scripts/qt_installer_windows.qs
-fi
