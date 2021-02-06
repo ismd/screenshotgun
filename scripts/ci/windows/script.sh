939a23742b47c654613b9c4f3f2cacb19c156537
Vladimir Kosteley <zzismd@gmail.com>
2021-02-07 02:29:19 +0300

    windows aqtinstall

--- scripts/ci/windows/script.sh
+++ scripts/ci/windows/script.sh
@@ -3,5 +3,5 @@
 mkdir $TRAVIS_BUILD_DIR/build
 cd $TRAVIS_BUILD_DIR/build
 
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/c/Qt/5.13.2/mingw73_64 ..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/c/Qt/5.15.2/mingw81_64 ..
 cmake --build . --config Release
