b03750d3b1e182bb89cece28516c4e6b5bbdbd30
Vladimir Kosteley <zzismd@gmail.com>
2021-02-07 13:08:50 +0300

    ifw windows

--- scripts/ci/windows/install.sh
+++ scripts/ci/windows/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Qt
+if [ -z "$(ls -A /c/Qt)" ]; then
+    choco install pip
+    py -m pip install --upgrade pip
+    py -m pip install aqtinstall
+    py -m aqt install -O /c/Qt 5.15.2 windows desktop win64_mingw81
+fi
+
 # Qt Installer Framework
-if [ -z "$(ls -A $HOME/qt-installer-framework/build)" ]; then
-    tar xf $HOME/qt-installer-framework-opensource-src-3.2.2.tar.gz -C /c/qt-installer-framework
-
-    mkdir /c/qt-installer-framework/build
-    cd /c/qt-installer-framework/build
-
-    qmake CONFIG+=release ../installerfw.pro
-    make
+if [ -z "$(ls -A /c/qt-installer-framework)" ]; then
+    py -m aqt tool -O /c/qt-installer-framework windows tools_ifw 4.0 qt.tools.ifw.40
 fi
