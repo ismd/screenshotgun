356aa3b3564f67d35c0d87532ce7e6b8e081bb04
Vladimir Kosteley <zzismd@gmail.com>
2021-02-09 19:53:34 +0300

    Fix cache

--- scripts/ci/windows/install.sh
+++ scripts/ci/windows/install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+if [ -z "$(ls -A /c/Qt)" ]; then
+    choco install pip
+    py -m pip install --upgrade pip
+    py -m pip install aqtinstall
+    py -m aqt install --outputdir "$HOME/Qt" 5.15.2 windows desktop win64_mingw81
+fi
